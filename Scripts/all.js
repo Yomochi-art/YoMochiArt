c8eede34827e31a42ce64af1da103bc8683eae95
Yomochi-art <yomogi.2018.0827@gmail.com>
2020-08-27 16:14:11 +0900

    add reload

--- Scripts/all.js
+++ Scripts/all.js
@@ -18,4 +18,38 @@ $(function() {
         startPos = winScrollTop;
     });
     */
+
+
+    function keep_scroll_reload() {
+      //コピペ：https://qiita.com/tukiyo3/items/7f79d8217a0e46ecc64f
+      // リロード TODO: ?が無い場合対応
+      var re = /&page_x=(\d+)&page_y=(\d+)/;
+      var page_x = document.documentElement ? document.documentElement.scrollLeft : document.body.scrollLeft;
+      var page_y = document.documentElement ? document.documentElement.scrollTop : document.body.scrollTop;
+      var position = '&page_x=' + page_x + '&page_y=' + page_y;
+      if(!url.match(re)) {
+              //初回
+              location.href = url + position;
+      } else {
+              //2回目以降
+              location.href = url.replace(/&page_x=(\d+)&page_y=(\d+)/,position);
+      }
+    }
+
+    // スクロール位置を復元
+    function restore_scroll() {
+      var re = /&page_x=(\d+)&page_y=(\d+)/;
+      if(window.location.href.match(re)) {
+              var position = window.location.href.match(re)
+              window.scrollTo(position[1],position[2]);
+      }
+    }
+
+    (window.onload = function() {
+          restore_scroll();
+    })();
+
+    $(window).on("resize", function() {
+      location.reload();
+    });
 });
